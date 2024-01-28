ece1ae4c80121af14943c26fc26ef94b2dc52338
Andrii Nalyaiko <videoedit.promo@gmail.com>
2024-01-28 10:26:41 +0000

    up

--- js/leson-3.js
+++ js/leson-3.js
@@ -59,17 +59,60 @@
 // Напишіть функцію, яка розгортає масив у зворотному порядку.
 // Будь ласка, не використовуйте array.reverse(), щоб зробити завдання цікавішим.
 
-const data = [10, 20, 30, 40, 50, 60];
+// const data = [10, 20, 30, 40, 50, 60];
 
-function reverse(array){
-  return array.reduce((acc, item) => [item, ...acc], [])
+// function reverse(array){
+//   return array.reduce((acc, item) => [item, ...acc], [])
 
-  // const newArr = [];
-  // for (let i = array.length - 1; i >= 0; i--) {
-  //   newArr.push(array[i])
-  // }
-  // return newArr;
-};
+//   // const newArr = [];
+//   // for (let i = array.length - 1; i >= 0; i--) {
+//   //   newArr.push(array[i])
+//   // }
+//   // return newArr;
+// };
 
-console.log(reverse(data));
+// console.log(reverse(data));
 
+// /3. Напиши класс Notes який управляє коллекцієй нотаток у
+//властивості items.
+//Нотатка це  об'єкт з властивостями text, priority
+//Додай класу статичну властивість Priopity,
+//в якій буде зберігатись об'єкт з пріорітетами ("hight", "middle", "low").
+//Додай методи addNote(note), removeNote(noteText)
+//updatePriority(noteText, newPriority)
+
+class Notes {
+  static Priority = {
+    HIGHT: "hight",
+    MIDDLE: "middle",
+    LOW: "low",
+  }
+  constructor() {
+    this.items = [];
+}
+  addNote(note) {
+    this.items.push(note);
+}
+  removeNote(noteText) {
+    this.items = this.items.filter(note => note.text !== noteText)  
+  }
+  
+  updatePriority(noteText, newPriority) {
+    const note = this.items.find(note => note.text === noteText)
+    if (note) {
+      note.priority = newPriority
+    }
+  }
+  
+}
+
+const notes = new Notes();
+notes.addNote({ text: "Hello, World", priority: Notes.Priority.HIGHT})
+notes.addNote({ text: "I studied English", priority: Notes.Priority.LOW})
+notes.addNote({ text: "I have e motobike", priority: Notes.Priority.MIDDLE })
+notes.removeNote("Hello, World")
+notes.updatePriority("I studied English", Notes.Priority.HIGHT)
+
+
+
+console.log(notes)
